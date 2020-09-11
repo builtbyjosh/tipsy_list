6e5128616192d53d2f232ca65d41c2d1fc34606e
builtbyjosh <admin@builtbyjosh.com>
2020-09-11 15:01:41 -0400

    set up API

--- lib/adapter.rb
+++ lib/adapter.rb
@@ -1,3 +1,26 @@
+https://sandbox-api.brewerydb.com/v2/
+
+
 class API
 
+    
+    BREWERY_URL='https://sandbox-api.brewerydb.com/v2/breweries' 
+    BEER_URL = 'https://sandbox-api.brewerydb.com/v2/brewery/'
+
+    def fetch_breweries
+        url = BREWERY_URL + '/?key=' + API_KEY 
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+
+        data = JSON.parse(response.body)
+
+    end
+    def fetch_beers
+        url = BEER_URL + brewery_id + "/beers" + '/?key=' + API_KEY
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+
+        data = JSON.parse(response.body)
+
+    end
 end
---
+++ lib/config/environment.rb
@@ -0,0 +1,12 @@
+
+require 'bundler/setup'
+Bundler.require
+
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+import api_key.rb
+
+
+require_all '../lib'
