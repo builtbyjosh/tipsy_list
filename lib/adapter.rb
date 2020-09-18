339d2ffb8066e8dbf9303716a68d51b92d66a747
builtbyjosh <admin@builtbyjosh.com>
2020-09-18 13:31:55 -0400

    Refactored API class

--- lib/adapter.rb
+++ lib/adapter.rb
@@ -5,23 +5,40 @@ class API
 
     BEER_URL = 'https://sandbox-api.brewerydb.com/v2/beers/'
 
-    def fetch_data
+    def fetch_data_beer
         url = BEER_URL + '/?key=' + API_KEY + "&withBreweries=Y"
         uri = URI.parse(url)
         body = uri.read        
         brewery = JSON.parse(body)
-        brewery["data"].each do |e|
-            temp_brew = 
-            if e["style"]
-                name = e["name"]
-                description =  e["style"]["description"]
-                style = Style.new(e["style"]['shortName'])
-                brewery = Brewery.new(e["breweries"][0]["name"])
-                Beer.new(name, description, style, brewery)
-            #binding.pry  
+        beers_hash = []
+        brewery["data"].each do |e|             
+            if e["style"]                
+                beers_hash << {name: e["name"], abv: e["abv"], description: e["style"]["description"], style: e["style"]['shortName'], brewery: e["breweries"][0]["nameShortDisplay"]}                
             end
-        end       
+        end
+        beers_hash
     end
+    
+    def fetch_data_brewery
+        url = BEER_URL + '/?key=' + API_KEY + "&withBreweries=Y"
+        uri = URI.parse(url)
+        body = uri.read        
+        brewery = JSON.parse(body)
+        brewery_hash = []
+        brewery["data"].each do |e|
+            website = e["breweries"][0]["website"]                
+            brewery_name = e["breweries"][0]["nameShortDisplay"]
+            established = e["breweries"][0]["established"]
+            street_address = e["breweries"][0]["locations"][0]["streetAddress"]
+            locality = e["breweries"][0]["locations"][0]["locality"]
+            region = e["breweries"][0]["locations"][0]["region"]
+            zip = e["breweries"][0]["locations"][0]["postalCode"]
+
+            brewery_hash << {name: brewery_name, website: website, established: established, street_address: street_address, locality: locality, region: region, zip: zip}
+        end        
+        brewery_hash.uniq
+    end
+
 end
 
 
@@ -29,5 +46,5 @@ end
 # brewery["data"][0]["style"]["shortName"] = style
 # brewery["data"][0]["style"]["description"] = description
 # brewery["data"][0]["breweries"][0]["names"] = brewery name
-# brewery["data"][0]["breweries"][0]["website"] = brewery website
+
 
