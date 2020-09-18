7b227c26e7661b4a5324e199bd2d04f8b84bb110
builtbyjosh <admin@builtbyjosh.com>
2020-09-18 13:35:20 -0400

    Refactored CLI Class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,12 @@
 class CLI
+    @@beers = []
+    @@breweries = []
 
     def main        
         fetch_beer_info
         greet_user
         #binding.pry
+        list_breweries
         menu        
         say_goodbye
         
@@ -28,7 +31,10 @@ class CLI
     end
 
     def fetch_beer_info
-        API.new.fetch_data
+        beers_hash = API.new.fetch_data_beer
+        brewery_hash = API.new.fetch_data_brewery
+        @@beers = beers_hash.map {|beers_hash| Beer.new(beers_hash)}
+        @@breweries = brewery_hash.map {|brewery_hash| Brewery.new(brewery_hash)}        
     end    
 
     def say_goodbye
@@ -36,13 +42,12 @@ class CLI
     end
 
     def menu      
-        puts "What would you like to know?"
-        puts ""
-        puts "Choose menu number:"
-        puts "  1. To list all Breweries."
-        puts "  2. To list all Beers."
-        puts "  3. To see a beer list for a specific Brewery."
-        puts "  4. To see more info on a specific Beer."
+        puts "Choose menu number for more information:"
+        puts "  1. Get more information on a specific Brewery?"        
+        puts "  2. Show a beer list for a specific Brewery."
+        puts "  3. See more info on a specific Beer."        
+        puts "  4. Show the brewery list again."        
+        puts "  5. See a list of all available beers."        
         puts ""
         puts "To quit, type 'exit'."
         puts ""
@@ -51,13 +56,19 @@ class CLI
 
         
         if input == "1"
-            list_breweries
+            brewery_info
         elsif input == "2"
-            list_beers
-        elsif input == "3"
             beers_at_brewery
-        elsif input == "4"
+        elsif input == "3"
             beer_info
+        elsif input == "4"
+            list_breweries
+        elsif input == "5"
+            list_all_beers
+        elsif input == "exit"
+            input = "exit"
+        else
+            puts "Invalid Choice, please try again."
         end
 
         unless input == "exit"
@@ -67,11 +78,17 @@ class CLI
     end
 
     def list_breweries
-        Brewery.all.each {|b| puts "    - #{b.name}" } 
-        beers_at_brewery
+        puts "Here is list of available breweries."
+        Brewery.all.each {|b| puts "    - #{b.name}" }
+        puts ""
     end
 
-    def list_beers
+    def list_beers(brewery_name)
+        Beer.all.each {|b| puts "   - #{b.name}" if b.brewery == brewery_name}
+        beer_info
+    end
+
+    def list_all_beers
         Beer.all.each {|b| puts "   - #{b.name}"}
         beer_info
     end
@@ -82,9 +99,9 @@ class CLI
         input = gets.chomp
 
         Beer.all.each do |b|
-            puts "  - #{b.name}" if b.brewery.name == input
+            puts "  - #{b.name}" if b.brewery == input
         end
-        beer_info
+        beer_info        
     end
 
     def beer_info
@@ -94,24 +111,55 @@ class CLI
         input = gets.chomp
         puts ""
 
-        Beer.all.each do |b|
+        Beer.all.select do |b|
             if b.name == input
                 puts "Name:     #{b.name}"
                 puts ""
                 puts "ABV:      #{b.abv}"
                 puts ""
-                puts "Style:    #{b.style.name}"
+                puts "Style:    #{b.style}"
                 puts ""
-                puts "Brewery:  #{b.brewery.name}"
-                puts ""
-                puts "Website:  #{b.brewery.website}"
-                puts ""                                
+                puts "Brewery:  #{b.brewery}"
+                puts ""                
                 puts "Description:"
                 puts ""
                 puts "          #{b.description}"
-                puts ""
+                puts ""            
             end
+
         end
     end
+
+    def brewery_info
+        puts ""
+        puts "What Brewery would you like more information on?"
+        puts ""
+        input = gets.chomp
+        puts ""
+
+        Brewery.all.each do |b|
+            if b.name == input
+                puts "Name:        #{b.name}"
+                puts ""
+                puts "Website:     #{b.website}"
+                puts ""
+                puts "Established: #{b.established}"
+                puts ""
+                puts "Address:     #{b.street_address}"
+                puts "             #{b.locality}, #{b.region} #{b.zip}"
+            end
+        end
+        puts ""
+        puts "Would you like to see the beer list for this brewery? (Y/N)"
+        input2 = gets.chomp.downcase
+        if input2 == "y"
+            list_beers(input)
+        else
+            menu
+        end
+    end
+
 end
+
+
 
